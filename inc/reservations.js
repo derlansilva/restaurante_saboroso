d44c5e6f89dd683e9a3e19d5f2ea10788ee9539d
derlansilva <joderlan.silva@gmail.com>
2020-08-02 14:33:32 -0300

    4ª commit

---
+++ inc/reservations.js
@@ -0,0 +1,49 @@
+const Connection = require("./db");
+const { fileLoader } = require("ejs");
+
+module.exports ={
+
+    render(req , res , error , success ){
+
+        res.render('reservations' , {
+            title : 'Reservas - Saboroso!',
+            background : 'images/img_bg_2.jpg',
+            h1: 'Reserve uma mesa!',
+            body : req.body,
+            error,
+            success 
+        });
+    },
+
+    save(fields){
+
+        return new Promise(( resolve , reject ) => {
+
+            let date = fields.date.split('/')
+
+            fields.date = `${date[2]}-${date[1]}-${date[0]}`
+
+            Connection.query(`
+            INSERT INTO tb_reservations (name , email , people , date , time)
+            VALUES(? ,?,?,?,?)
+            ` , [
+                fields.name,
+                fields.email,
+                fields.people,
+                fields.date,
+                fields.time
+            ] , (err , results) => {
+                
+                if(err){
+                    reject(err)
+                }else{
+                    resolve(results)
+                }
+
+            })
+
+        })
+
+    }
+
+}
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const menus =  require("../inc/menus")
 
-const connection = require('../inc/db')
+const connection = require('../inc/db');
+const reservations = require('../inc/reservations');
+const contacts = require('../inc/contacts');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +13,8 @@ router.get('/', function(req, res, next) {
 
     res.render("index" , {
       title: "Saboroso!" ,
-      menus : results
+      menus : results ,
+      isHome : true
     })
 
   })
@@ -20,11 +23,41 @@ router.get('/', function(req, res, next) {
 
 
 router.get("/contacts" , function(req , res , next){
-  res.render('contacts', {
-    title : 'Saboroso',
-    background: 'images/img_bg_3.jpg',
-    h1:'Diga um oi!'
-  })
+
+  contacts.render(req , res);
+
+})
+
+router.post('/contacts' , (req , res , next ) => {
+
+  if(!req.body.name){
+
+    contacts.render(req , res , 'Obrigatorio o nome')
+
+  }
+  else if(!req.body.email){
+
+    contacts.render(req , res , 'Obrigatorio o email')
+
+  }
+  else if(!req.body.message){
+
+    contacts.render(req , res , 'Digite sua messagem')
+
+  }else {
+
+    contacts.save(req.body).then(results => {
+
+      contacts.render(req , res , null , "Contato enviado com sucesso")
+
+    }).catch(err => {
+
+      contacts.render(req , res  , err.message)
+
+    })
+
+  }
+
 })
 
 router.get('/menus' , (req , res ,next )=>{
@@ -41,14 +74,51 @@ router.get('/menus' , (req , res ,next )=>{
     })
 })
 
+//exibindo dados com o metodo get
 router.get('/reservations' , (req, res , next ) => {
-    res.render('reservations' ,{
-      title : 'Saboroso',
-      background: 'images/img_bg_2.jpg',
-      h1:'Reserve uma Mesa!'
-    
-    })
+
+  reservations.render(req , res )
+
 })
+
+//envio de dados com o metodo post para a base de dados 
+router.post('/reservations' , (req ,res , next) => {
+
+  if(!req.body.name){
+
+    reservations.render(req , res , "Digite o nome")
+
+  }else if(!req.body.email){
+    
+    reservations.render(req , res , "Coloque seu email para fazer uma reserva")
+
+  }else if(!req.body.people){
+
+    reservations.render(req , res, "Preencha quantas pessoas")
+
+  }else if(!req.body.date){
+
+    reservations.render(req ,  res , "Selecione a data que deseja fazer a reserva")
+
+  }else if(!req.body.time){
+
+    reservations.render(req , res , "Selecione a hora da que deseja fazer a reserva")
+
+  }else {
+
+    reservations.save(req.body).then(results => {
+
+        req.body = {}
+
+        reservations.render(req , res , null , "Reserva realizada com sucesso")
+
+    }).catch(err => {
+        reservations.render(req , res , err.message)
+    })
+  }
+
+})
+
 
 router.get('/services' , (req , res , next ) => {
     res.render("services" , {
@@ -57,4 +127,6 @@ router.get('/services' , (req , res , next ) => {
       h1:'É um prazer poder servir!'
     })
 })
+
+
 module.exports = router;
